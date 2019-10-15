ace0df0ad6cedbd7810a624f9e691e46c8c5fff2
mirarodd548 <mirarodd548@SNS-D-130S00>
2019-10-15 13:08:21 -0400

    Add project files.

---
+++ CashMoney/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CashMoney
+{
+    partial class cashMoney
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.strawberryJam = new System.Windows.Forms.Label();
+            this.bananaJam = new System.Windows.Forms.Label();
+            this.coconutJam = new System.Windows.Forms.Label();
+            this.strawberryjamInput = new System.Windows.Forms.TextBox();
+            this.bananajamInput = new System.Windows.Forms.TextBox();
+            this.coconutjamInput = new System.Windows.Forms.TextBox();
+            this.subTotal = new System.Windows.Forms.Label();
+            this.tax = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            this.calculateValue = new System.Windows.Forms.Button();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.printRecipt = new System.Windows.Forms.Button();
+            this.line = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // strawberryJam
+            // 
+            this.strawberryJam.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.strawberryJam.Font = new System.Drawing.Font("Mixage Bk BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strawberryJam.ForeColor = System.Drawing.SystemColors.Control;
+            this.strawberryJam.Location = new System.Drawing.Point(45, 38);
+            this.strawberryJam.Name = "strawberryJam";
+            this.strawberryJam.Size = new System.Drawing.Size(134, 24);
+            this.strawberryJam.TabIndex = 0;
+            this.strawberryJam.Text = "Strawberry Jam ";
+            // 
+            // bananaJam
+            // 
+            this.bananaJam.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bananaJam.Font = new System.Drawing.Font("Mixage Bk BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bananaJam.ForeColor = System.Drawing.SystemColors.Control;
+            this.bananaJam.Location = new System.Drawing.Point(45, 62);
+            this.bananaJam.Name = "bananaJam";
+            this.bananaJam.Size = new System.Drawing.Size(134, 31);
+            this.bananaJam.TabIndex = 1;
+            this.bananaJam.Text = "Banana Jam ";
+            // 
+            // coconutJam
+            // 
+            this.coconutJam.BackColor = System.Drawing.SystemColors.ControlText;
+            this.coconutJam.Font = new System.Drawing.Font("Mixage Bk BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coconutJam.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.coconutJam.Location = new System.Drawing.Point(45, 90);
+            this.coconutJam.Name = "coconutJam";
+            this.coconutJam.Size = new System.Drawing.Size(134, 27);
+            this.coconutJam.TabIndex = 2;
+            this.coconutJam.Text = "Coconut Jam";
+            // 
+            // strawberryjamInput
+            // 
+            this.strawberryjamInput.BackColor = System.Drawing.Color.Gold;
+            this.strawberryjamInput.ForeColor = System.Drawing.Color.Black;
+            this.strawberryjamInput.Location = new System.Drawing.Point(194, 38);
+            this.strawberryjamInput.Name = "strawberryjamInput";
+            this.strawberryjamInput.Size = new System.Drawing.Size(100, 20);
+            this.strawberryjamInput.TabIndex = 3;
+            // 
+            // bananajamInput
+            // 
+            this.bananajamInput.BackColor = System.Drawing.Color.Gold;
+            this.bananajamInput.ForeColor = System.Drawing.Color.Black;
+            this.bananajamInput.Location = new System.Drawing.Point(194, 64);
+            this.bananajamInput.Name = "bananajamInput";
+            this.bananajamInput.Size = new System.Drawing.Size(100, 20);
+            this.bananajamInput.TabIndex = 4;
+            // 
+            // coconutjamInput
+            // 
+            this.coconutjamInput.BackColor = System.Drawing.Color.Gold;
+            this.coconutjamInput.ForeColor = System.Drawing.Color.Black;
+            this.coconutjamInput.Location = new System.Drawing.Point(194, 90);
+            this.coconutjamInput.Name = "coconutjamInput";
+            this.coconutjamInput.Size = new System.Drawing.Size(100, 20);
+            this.coconutjamInput.TabIndex = 5;
+            // 
+            // subTotal
+            // 
+            this.subTotal.AutoSize = true;
+            this.subTotal.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.subTotal.Font = new System.Drawing.Font("Calligraph421 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotal.ForeColor = System.Drawing.Color.White;
+            this.subTotal.Location = new System.Drawing.Point(28, 169);
+            this.subTotal.Name = "subTotal";
+            this.subTotal.Size = new System.Drawing.Size(72, 16);
+            this.subTotal.TabIndex = 6;
+            this.subTotal.Text = "Sub Total";
+            // 
+            // tax
+            // 
+            this.tax.AutoSize = true;
+            this.tax.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tax.Font = new System.Drawing.Font("Calligraph421 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tax.ForeColor = System.Drawing.SystemColors.Window;
+            this.tax.Location = new System.Drawing.Point(42, 196);
+            this.tax.Name = "tax";
+            this.tax.Size = new System.Drawing.Size(37, 20);
+            this.tax.TabIndex = 7;
+            this.tax.Text = "Tax";
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.total.Font = new System.Drawing.Font("Caslon Bd BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total.ForeColor = System.Drawing.Color.White;
+            this.total.Location = new System.Drawing.Point(39, 229);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(40, 16);
+            this.total.TabIndex = 8;
+            this.total.Text = "Total";
+            // 
+            // calculateValue
+            // 
+            this.calculateValue.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.calculateValue.Font = new System.Drawing.Font("kroeger 07_66", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateValue.ForeColor = System.Drawing.Color.SteelBlue;
+            this.calculateValue.Location = new System.Drawing.Point(31, 134);
+            this.calculateValue.Name = "calculateValue";
+            this.calculateValue.Size = new System.Drawing.Size(194, 25);
+            this.calculateValue.TabIndex = 9;
+            this.calculateValue.Text = "Calculate Value ";
+            this.calculateValue.UseVisualStyleBackColor = false;
+            this.calculateValue.Click += new System.EventHandler(this.calculateValue_Click);
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.BackColor = System.Drawing.Color.Gold;
+            this.subtotalOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.subtotalOutput.Location = new System.Drawing.Point(105, 169);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(100, 16);
+            this.subtotalOutput.TabIndex = 10;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.BackColor = System.Drawing.Color.Gold;
+            this.taxOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.taxOutput.Location = new System.Drawing.Point(105, 196);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(100, 20);
+            this.taxOutput.TabIndex = 11;
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.BackColor = System.Drawing.Color.Gold;
+            this.totalOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.totalOutput.Location = new System.Drawing.Point(105, 229);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(100, 16);
+            this.totalOutput.TabIndex = 12;
+            // 
+            // printRecipt
+            // 
+            this.printRecipt.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.printRecipt.Font = new System.Drawing.Font("kroeger 05_63", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printRecipt.ForeColor = System.Drawing.Color.SteelBlue;
+            this.printRecipt.Location = new System.Drawing.Point(12, 282);
+            this.printRecipt.Name = "printRecipt";
+            this.printRecipt.Size = new System.Drawing.Size(228, 37);
+            this.printRecipt.TabIndex = 13;
+            this.printRecipt.Text = "Print Recipt";
+            this.printRecipt.UseVisualStyleBackColor = false;
+            this.printRecipt.Click += new System.EventHandler(this.printRecipt_Click);
+            // 
+            // line
+            // 
+            this.line.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.line.Location = new System.Drawing.Point(25, 247);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(190, 11);
+            this.line.TabIndex = 20;
+            // 
+            // cashMoney
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(889, 453);
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.printRecipt);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.calculateValue);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.tax);
+            this.Controls.Add(this.subTotal);
+            this.Controls.Add(this.coconutjamInput);
+            this.Controls.Add(this.bananajamInput);
+            this.Controls.Add(this.strawberryjamInput);
+            this.Controls.Add(this.coconutJam);
+            this.Controls.Add(this.bananaJam);
+            this.Controls.Add(this.strawberryJam);
+            this.Name = "cashMoney";
+            this.Text = "Jayden\'s Jams";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label strawberryJam;
+        private System.Windows.Forms.Label bananaJam;
+        private System.Windows.Forms.Label coconutJam;
+        private System.Windows.Forms.TextBox strawberryjamInput;
+        private System.Windows.Forms.TextBox bananajamInput;
+        private System.Windows.Forms.TextBox coconutjamInput;
+        private System.Windows.Forms.Label subTotal;
+        private System.Windows.Forms.Label tax;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Button calculateValue;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Button printRecipt;
+        private System.Windows.Forms.Label line;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ CashMoney/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; 
+
+namespace CashMoney
+    /* 
+     * Jayden Roddick 
+     * Cash Rigister 
+     * 10/07/19
+     */ 
+{
+    public partial class cashMoney : Form
+    {
+        //global
+       const  double strawberryjamPrice = 3;
+        const double bananajamPrice = 7.50;
+        const double coconutjamPrice = 10;
+
+        int numberofstwaberryjamInput;
+        int numberofbananaofjamInput;
+        int numberofcoconutjamInput;
+
+        double subTotalInput;
+       const double taxTotal = 0.13; 
+        double totalPrice;
+        double taxtotalDisplay; 
+        public cashMoney()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateValue_Click(object sender, EventArgs e)
+        {
+            numberofstwaberryjamInput = Convert.ToInt16(strawberryjamInput.Text);
+            numberofbananaofjamInput = Convert.ToInt16(bananajamInput.Text);
+            numberofcoconutjamInput = Convert.ToInt16(coconutjamInput.Text);
+
+            subTotalInput = numberofstwaberryjamInput * strawberryjamPrice + numberofbananaofjamInput * bananajamPrice + numberofcoconutjamInput * coconutjamPrice;
+            taxtotalDisplay = numberofbananaofjamInput * taxTotal + numberofcoconutjamInput * taxTotal + numberofstwaberryjamInput * taxTotal; 
+            totalPrice = subTotalInput + taxtotalDisplay;
+            subtotalOutput.Text = subTotalInput.ToString("C");
+            taxOutput.Text = taxtotalDisplay.ToString("C");
+            totalOutput.Text = totalPrice.ToString("C");  
+                 
+
+        }
+
+        private void printRecipt_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+            Font drawFont = new Font("Arial", 12, FontStyle.Bold);
+            SolidBrush drawBr= new SolidBrush(Color.Black);
+            drawBrush.Color = Color.Red;
+            g.FillRectangle(drawBrush, 500, 90, 220, 330);
+            g.DrawString("Jayden's Jams", drawFont, drawBr, 500, 90);
+            Thread.Sleep(1000);
+            g.DrawString("10/15/19", drawFont, drawBr, 500, 110);
+            Thread.Sleep(1000);
+            g.DrawString("Strawberry Jam x " + numberofstwaberryjamInput, drawFont, drawBr, 500, 130);
+            Thread.Sleep(1000);
+            g.DrawString("Banana Jam x " + numberofbananaofjamInput, drawFont, drawBr, 500, 150);
+            Thread.Sleep(1000);
+            g.DrawString("Coconut Jam x " + numberofcoconutjamInput, drawFont, drawBr, 500, 170);
+            Thread.Sleep(1000);
+            g.DrawString("SubTotal " + subTotalInput.ToString("C"), drawFont, drawBr, 500, 220);
+            Thread.Sleep(1000);
+            g.DrawString("Tax " + taxtotalDisplay.ToString("C"), drawFont, drawBr, 500, 240);
+            Thread.Sleep(1000);
+            g.DrawString("Total " + totalPrice.ToString("C"), drawFont, drawBr, 500, 260);
+            Thread.Sleep(1000);
+            g.DrawString("Have a nice day", drawFont, drawBr, 500, 280);
+
+        }
+    }
+}
+
+
